6bba8fdce99b90973352d768e66af0291726d0c9
Nertil Poci <nertilpoci@hotmail.com>
2025-05-08 15:03:59 +0200

    update

--- stencil.config.ts
+++ stencil.config.ts
@@ -17,8 +17,8 @@ export const config: Config = {
     },
     {
       type: 'www',
+      dir: 'www',
       serviceWorker: null,
-      baseUrl: 'https://nertilpoci.github.io/signiturebuilderwebcomponents/signiturebuilderwebcomponents/', 
       copy: [
         {
           src: '../node_modules/pdfjs-dist/build/pdf.worker.min.mjs',
